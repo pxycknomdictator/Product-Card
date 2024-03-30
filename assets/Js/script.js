b1deb4006e0a95ee50bd895b4f295879694b1e94
Noman <pxycknomdictator@outlook.com>
2024-03-31 03:35:33 +0500

    Added some JavaScript Login & dropdown-menu

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,3 +1,5 @@
+// menu bar script
+
 const menuButton = document.querySelector("#menubtn");
 const crossMenuButton = document.querySelector("#cross");
 
@@ -8,3 +10,32 @@ menuButton.addEventListener("click", () => {
 crossMenuButton.addEventListener("click", () => {
     document.querySelector("#list").style.left = "-400px"
 })
+
+// increament and decreament script
+
+const increamentContainer = document.querySelector("#quantity-boxx");
+let number = document.querySelector("#number");
+let counter = 0;
+
+const checkquantity = (value) => {
+    return (value >= 0) ? value++ : 0;
+}
+
+increamentContainer.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    try {
+        if (event.target.className === "plus") {
+            counter += 1
+        } else if (event.target.className === "minus") {
+            counter -= 1
+        } else {
+            return false
+        }
+        number.innerText = (checkquantity(counter));
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
